942a6fad174c684d8b23cf5dce498d15e6e7304e
Blob2763 <88489444+Blob2763@users.noreply.github.com>
2023-07-25 13:53:45 +0100

    Update json.js

--- json.js
+++ json.js
@@ -1,4 +1,4 @@
-funstion testJSON() {
+function testJSON() {
     fetch('https://blob2763.github.io/utils/tools.json')
         .then((response) => response.json())
         .then((json) => console.log(json));
